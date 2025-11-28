53d9e5c518efb8df50334edff7f8f3453ec96182
ibrahimamar07 <ibrahimamaralfanani07@gmail.com>
2025-11-28 19:00:05 +0700

    integrasi supabase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
 // lib/main.dart
 import 'package:flutter/material.dart';
 import 'app.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Supabase.initialize(
+    url: 'https://apcthhdmbegbkgtallpm.supabase.co',
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFwY3RoaGRtYmVnYmtndGFsbHBtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQzMjU5NzksImV4cCI6MjA3OTkwMTk3OX0.wqDQD8oaYN4dtRzSuKNRgXYUd-2QnQclsVoaJhd7I9w',
+  );
+
   runApp(const RestokInApp());
 }
+
+final supabase = Supabase.instance.client;
