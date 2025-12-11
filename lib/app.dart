53167df6af39a8fc5fd52ad0c62cc2a394a0c076
Meltistry <jmelvinch@gmail.com>
2025-12-11 18:05:24 +0700

    Revert "Reapply "Merge branch 'master' of https://github.com/Meltistry/Restok.in""
    This reverts commit 13fc556409a11f71362c86789a30a436712daf6d.

--- lib/app.dart
+++ lib/app.dart
@@ -2,9 +2,6 @@
 import 'package:flutter/material.dart';
 import 'core/theme/app_theme.dart';
 import 'features/auth/login_page.dart';
-import 'features/auth/register_page.dart';
-import 'features/profile/create_profile_page.dart';
-import 'features/store_owner/my_store_page.dart';
 
 class RestokInApp extends StatelessWidget {
   const RestokInApp({super.key});
@@ -17,13 +14,7 @@ class RestokInApp extends StatelessWidget {
       theme: AppTheme.light,
       darkTheme: AppTheme.dark,
       themeMode: ThemeMode.dark,
-      initialRoute: '/login',
-      routes: {
-        '/login': (_) => const LoginPage(),
-        '/register': (_) => const RegisterPage(),
-        '/create-profile': (_) => const CreateProfilePage(),
-        '/my-store': (_) => const MyStorePage(),
-      },
+      home: const LoginPage(),
     );
   }
 }
